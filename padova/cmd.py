e9ed10badb7dda828e1d2819f542ce7f645d8db1
Jonathan Sick <jonathansick@mac.com>
2013-08-13 11:21:36 -0500

    Add cmd module

---
+++ padova/cmd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Python interface to the Padova group's CMD web interface for isochrones.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
