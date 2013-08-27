e4f3c7abbcbef9730aa0da2cbd512a8524fd3a68
Jonathan Sick <jonathansick@mac.com>
2013-08-26 23:53:07 -0400

    PadovaCache is an interface to cached results

---
+++ padova/resultcache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+resultcache manages the cache of results from the CMD and TRILGEGAL
+interfaces. To be used internally.
+"""
+
+import os
+
+
+class PadovaCache(object):
+    """Cache manager for CMD and TRILEGAL requests."""
+    def __init__(self):
+        super(PadovaCache, self).__init__()
+        self._dir = os.expanduser("~/.padova_cache")
+        if not os.path.exists(self._dir):
+            os.makedirs(self._dir)
+
+    def cached_path(self, h):
+        """Returns path to cached data, returns `None` if not in cache."""
+        testpath = os.path.join(self._dir, h)
+        if os.path.exists(testpath):
+            return testpath
+        else:
+            return None
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
