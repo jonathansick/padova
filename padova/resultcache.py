1c9e275fccf9d0e8476aefd15fa2b791914f5d6a
Jonathan Sick <jonathansick@mac.com>
2015-04-15 14:41:59 -0400

    PadovaCache takes settings instances.

--- padova/resultcache.py
+++ padova/resultcache.py
@@ -9,27 +9,31 @@ import os
 
 
 class PadovaCache(object):
-    """Cache manager for CMD and TRILEGAL requests."""
+    """Cache manager for CMD and TRILEGAL requests.
+
+    The cache takes :class:`padova.settings.Settings` instances to hash
+    results in the cache.
+    """
     def __init__(self):
         super(PadovaCache, self).__init__()
         self._dir = os.path.expanduser("~/.padova_cache")
         if not os.path.exists(self._dir):
             os.makedirs(self._dir)
 
-    def _cache_path(self, h):
-        return os.path.join(self._dir, str(h))
+    def _cache_path(self, settings):
+        return os.path.join(self._dir, str(settings.__hash__()))
 
-    def __contains__(self, h):
-        return os.path.exists(self._cache_path(h))
+    def __contains__(self, settings):
+        return os.path.exists(self._cache_path(settings))
 
-    def __getitem__(self, h):
-        assert self.__contains__(h)
-        with open(self._cache_path(h)) as f:
+    def __getitem__(self, settings):
+        assert self.__contains__(settings)
+        with open(self._cache_path(settings)) as f:
             data = f.read()
         return data
 
-    def __setitem__(self, h, data):
-        p = self._cache_path(h)
+    def __setitem__(self, settings, data):
+        p = self._cache_path(settings)
         if os.path.exists(p):
             os.remove(p)
         with open(p, 'w') as f:
